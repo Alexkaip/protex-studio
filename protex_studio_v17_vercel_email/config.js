5497e28c6805d15c7b62d520d9a24608e557b89d
Alexkaip <Alex.kaip@gmx.at>
2026-06-19 19:01:46 +0200

    Delete protex_studio_v17_vercel_email directory

--- protex_studio_v17_vercel_email/config.js
+++
@@ -1,9 +0,0 @@
-// PROTEX Supabase Konfiguration
-// Publishable Key ist bereits eingetragen.
-// Wichtig: NICHT den Secret Key verwenden.
-
-window.PROTEX_CONFIG = {
-  SUPABASE_URL: "https://uzlfyoonzcdsbcxpvvwt.supabase.co",
-  SUPABASE_KEY: "sb_publishable_Tf7dcoLI_eAbkVXyr1DdSw_YkJYwSTJ",
-  STORAGE_BUCKET: "products"
-};
